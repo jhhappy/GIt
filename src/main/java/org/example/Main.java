11a644dec77eb2f09da0b1edae009fb4de6830a4
jjh <jh18438000868@qq.com>
2023-03-29 13:51:18 +0800

    fuck second

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,6 @@ public class Main {
         System.out.println("Hello Git1!");
         System.out.println("Hello Git2!");
         System.out.println("Hello Git3!");
-        System.out.println("Hello Git4!");
+        System.out.println("Git fuck!");
     }
 }
